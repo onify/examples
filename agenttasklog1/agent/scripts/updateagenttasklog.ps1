cc776d1526c444e7de9e0604df2534889c54d3c8
Robert Lundsten <robert@onify.co>
2018-10-29 11:06:09 +0100

    Fixed payload

--- agenttasklog1/agent/scripts/updateagenttasklog.ps1
+++ agenttasklog1/agent/scripts/updateagenttasklog.ps1
@@ -1,10 +1,10 @@
 param(
 	[Parameter(mandatory=$false)]
-	[string]$OnifyTaskID # The taskid to update logs for (default variable from the agent task)
+	[string]$onifytaskid # The taskid to update logs for (default variable from the agent task)
 )
 
 $config = Get-Content -Raw -Path "$PSScriptRoot\..\config.json" -Encoding "UTF8" | ConvertFrom-Json
-$onify_tasklog_uri = $config.hub_url + "/api/v1/my/agent/task/$OnifyTaskID/log"
+$onify_tasklog_uri = $config.hub_url + "/api/v1/my/agent/task/$onifytaskid/log"
 $onify_headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $onify_headers.Add("Authorization", $config.hub_token)
 
@@ -14,7 +14,7 @@ foreach($i in 1..10) {
     if ($i -eq 10) { $level = "error" }
     $body = @{
         message = "Testing $i"
-        info = $level
+        level = $level
     }
     $json = $body | ConvertTo-Json
     $r = Invoke-RestMethod -Method PUT -Uri $onify_tasklog_uri -Headers $onify_headers -Body ([System.Text.Encoding]::UTF8.GetBytes($json)) -ContentType application/json
